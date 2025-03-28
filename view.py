d342f1a681e63cf8bccbcb7c2aaf15216727492b
mokshhh20 <121478660+mokshhh20@users.noreply.github.com>
2025-03-28 10:20:45 +0530

    Update view.py

--- view.py
+++ view.py
@@ -21,4 +21,5 @@ def home():
     return render_template("index.html", data=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(debug=True, host='0.0.0.0', port=port)
